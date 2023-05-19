1205c5bafc626ea14af0af9b75bff7078617b500
jason <jason@192.168.2.49>
2023-05-19 10:37:37 -0400

    usah test

--- Test Suites/USAHockeytv.ts
+++ Test Suites/USAHockeytv.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>aa1cb603-c167-459f-a0de-22d9c0d9fe1d</testSuiteGuid>
+   <testCaseLink>
+      <guid>e13dd1f7-d677-4a8c-8c7d-9b92551fd153</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/buy page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
