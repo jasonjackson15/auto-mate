aaaa8452e4fdac71d2647328e32711782ea1c523
jason <jason@192.168.2.49>
2023-05-19 11:12:58 -0400

    tim tests

---
+++ Object Repository/div_Celular 50 aparelho ganha exposio inter_5c33c0.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Celular 50 aparelho ganha exposio inter_5c33c0</name>
+   <tag></tag>
+   <elementGuidId>6768d7e1-ea96-4632-81ce-016bef4ee3eb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='thresholdChecker']/div[2]/div[3]/a/article/div/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>4af75d11-0c70-4625-ab92-56c138144611</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>text-15 font-medium mobile:font-semibold text-['Inter'] mt-4 mobile:pr-4 tracking-normal overflow-hidden text-ellipsis h-20 text-textnew</value>
+      <webElementGuid>cbf6ec4a-61ec-4a0b-89f3-588147583503</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Celular 50: aparelho ganha exposição interativa no Museu do Amanhã</value>
+      <webElementGuid>964b0c6f-957e-4ed0-8a74-05c8772d83d3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;thresholdChecker&quot;)/div[@class=&quot;px-1 mb-12 desktop:grid desktop:grid-cols-4 desktop:gap-4 mb-5&quot;]/div[@class=&quot;my-2&quot;]/a[@class=&quot;desktop:hidden&quot;]/article[@class=&quot;desktop:w-full mobile:h-[159px] desktop:h-[147px]&quot;]/div[@class=&quot;flex flex-row h-full mobile:px-4&quot;]/div[@class=&quot;flex flex-col h-full mobile:w-full desktop:max-w-[225px] desktop:mr-4 mobile:border-b mobile:border-gray-300&quot;]/div[@class=&quot;text-15 font-medium mobile:font-semibold text-['Inter'] mt-4 mobile:pr-4 tracking-normal overflow-hidden text-ellipsis h-20 text-textnew&quot;]</value>
+      <webElementGuid>d589199d-7e55-4bce-ac6c-d2a144f7b89f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='thresholdChecker']/div[2]/div[3]/a/article/div/div/div</value>
+      <webElementGuid>7cca2f01-0d95-467d-be82-ea08b940615b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='...'])[5]/following::div[3]</value>
+      <webElementGuid>c9afd215-5352-4488-ba23-eae9f86c08e8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Celular 50: aparelho ganha exposição interativa no Museu do Amanhã'])[1]/following::div[10]</value>
+      <webElementGuid>d6a4cfed-8d2e-4b38-a2a8-b1f103fcee24</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='...'])[6]/preceding::div[5]</value>
+      <webElementGuid>d83078c3-ccf8-4acf-bc06-ab3f0433cd17</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/a/article/div/div/div</value>
+      <webElementGuid>383d4c9a-1397-4dca-9a8f-d801e19186ca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Celular 50: aparelho ganha exposição interativa no Museu do Amanhã' or . = 'Celular 50: aparelho ganha exposição interativa no Museu do Amanhã')]</value>
+      <webElementGuid>3bd76084-981d-4ba4-98b8-ef7110d3c150</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
