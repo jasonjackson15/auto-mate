d872c2e0747f50e040a814cb86f3960a33c26d02
jason <jason@192.168.2.49>
2023-05-19 09:49:54 -0400

    usah tv test run

---
+++ Test Suites/USAHockeytv.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>USAHockeytv</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>aa1cb603-c167-459f-a0de-22d9c0d9fe1d</testSuiteGuid>
+</TestSuiteEntity>
